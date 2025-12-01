cf16e5d4dc4a6e0c0a225973b25c05f97568a355
Andreas Hofmann <andreas@andreass-macbook-pro.home>
2025-12-01 18:36:06 +0100

    feat: add alternate background

--- src/components/section/section.tsx
+++ src/components/section/section.tsx
@@ -16,30 +16,30 @@ export function Section(props: SectionProps) {
   const { children, id, title, align = "left", adornment, hasMargin = true } = props;
 
   return (
-    <section
-      id={id}
-      className={clsx("mx-auto flex max-w-7xl flex-col px-6", {
-        "py-20": hasMargin,
-      })}
-    >
-      {(title || adornment) && (
-        <div
-          className={clsx({
-            "flex justify-center": align === "center",
-            "flex justify-end": align === "right",
-            "flex justify-between": adornment,
-          })}
-        >
-          {title && (
-            <Heading type="h2" marginBottom="xl">
-              {title}
-            </Heading>
-          )}
-          {adornment && adornment}
-        </div>
-      )}
-
-      {children}
+    <section id={id}>
+      <div
+        className={clsx("mx-auto flex max-w-7xl flex-col px-6", {
+          "py-20": hasMargin,
+        })}
+      >
+        {(title || adornment) && (
+          <div
+            className={clsx({
+              "flex justify-center": align === "center",
+              "flex justify-end": align === "right",
+              "flex justify-between": adornment,
+            })}
+          >
+            {title && (
+              <Heading type="h2" marginBottom="xl">
+                {title}
+              </Heading>
+            )}
+            {adornment && adornment}
+          </div>
+        )}
+        {children}
+      </div>
     </section>
   );
 }
